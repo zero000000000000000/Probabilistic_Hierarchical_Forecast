acd3de9ec586dcaab01a8bb208cd3ecbc2a23105
zero000000000000000 <15112548528@163.com>
2024-03-19 23:10:41 +0800

    Complete the baseline model evaluation on ES

--- simulation/Reconcile_and_Evaluation_R/reconcile_and_evaluation_r_code.R
+++ simulation/Reconcile_and_Evaluation_R/reconcile_and_evaluation_r_code.R
@@ -1,32 +1,14 @@
 #Create and Evaluate reconciled forecasts
 
+#install.packages("rjson")
+library(rjson)
 # Load library
 library(tidyverse)
 library(mvtnorm)
 library(Matrix)
-
 #Clear workspace
 rm(list=ls())
 
-
-evalN<-10 #Number of evaluation periods
-Q<-1000 #Number of draws to estimate energy score
-N<-500 #Training sample size
-M<-7 #Number of series
-
-#Set up S matrix
-S<-matrix(c(1,1,1,1,
-            1,1,0,0,
-            0,0,1,1,
-            1,0,0,0,
-            0,1,0,0,
-            0,0,1,0,
-            0,0,0,1),7,4,byrow = T)
-
-#Predefine some reconciliation matrices
-
-SG_bu<-S%*%cbind(matrix(0,4,3),diag(rep(1,4)))
-SG_ols<-S%*%solve(t(S)%*%S,t(S))
 
 #Energy score with alpha = 2
 energy_score<-function(y,x,xs){
@@ -57,59 +39,7 @@ shrink.estim <- function(res)
   return(W)
 }
 
-# Set the type of the input dataset
-innovationsj<-'gaussian'
-depj<-'independent'
-
-#Read in data
-data<-read.csv('D:\\HierarchicalCode\\simulation\\Data\\Simulated_Data_With_Added_Noise.csv')
-
-# Read the Base forecast results
-#install.packages("rjson")
-library(rjson)
-fc<-fromJSON(file = "D:\\HierarchicalCode\\simulation\\Base_Forecasts\\base_forecasts_results.json")
-for (i in 1:10){
-  names(fc[[i]])<-c('fc_mean','fc_var','resid','fitted')
-}
-
-# Calculate sd
-for (i in 1:10){
-  sd_list<-NULL
-  for (j in 1:7){
-    sd_list<-c(sd_list,sqrt(fc[[i]]$fc_var[j]))
-  }
-  fc[[i]]$fc_sd<-sd_list
-}
-
-
-for (i in 1:10){
-  k<-NULL
-  for (j in 1:7){
-    m<-matrix(fc[[i]]['resid'][[1]][[j]],nrow=1,ncol=500,byrow=T)
-    k<-rbind(k,m)
-  }
-  fc[[i]]$fc_Sigma_sam<-cov(t(k))
-  fc[[i]]$fc_Sigma_shr<-shrink.estim(t(k))
-}
-
-# Without immutable series
-Base<-rep(NA,evalN)
-BottomUp<-rep(NA,evalN)
-OLS<-rep(NA,evalN)
-WLS<-rep(NA,evalN)
-JPP<-rep(NA,evalN)
-MinTShr<-rep(NA,evalN)
-MinTSam<-rep(NA,evalN)
-BTTH<-rep(NA,evalN)
-Im<-rep(NA,evalN)
-
-# With immutable series
-OLSv<-rep(NA,evalN)
-WLSv<-rep(NA,evalN)
-MinTShrv<-rep(NA,evalN)
-MinTSamv<-rep(NA,evalN)
-
-# produce summing matrix of new basis time series
+# Produce summing matrix of new basis time series
 transform.sMat <- function(sMat, basis_set){
   m <- dim(sMat)[2]
   if (length(basis_set) != m){
@@ -121,19 +51,12 @@ transform.sMat <- function(sMat, basis_set){
   rbind(S2 %*% transitionMat, diag(rep(1, m)))
 }
 
-forecast.reconcile <- function(base_forecasts, 
-                               sMat,
-                               weighting_matrix,
-                               immu_set=NULL,
-                               nonnegative=FALSE){
+# Return basis series index,that is vector v and u
+forecast.basis_series <- function(sMat,
+                                  immu_set=NULL){
   m <- dim(sMat)[2]
   n <- dim(sMat)[1]
   k <- length(immu_set)
-  if (length(immu_set) == 0){
-    weighting_matrix = solve(weighting_matrix)
-    reconciled_y = sMat %*% solve(t(sMat) %*% weighting_matrix %*% sMat) %*% t(sMat) %*% weighting_matrix %*% t(base_forecasts)
-    return(t(reconciled_y))
-  }
   
   # construct new basis time series
   if (k > m) {
@@ -165,38 +88,110 @@ forecast.reconcile <- function(base_forecasts,
       }
     }
   }
+  return(list(mutable_basis=mutable_basis,immutable_basis=immutable_basis))
+}
+
+
+# Return the reconciled result
+forecast.reconcile<-function(base_forecasts,
+                             sMat,
+                             weighting_matrix,
+                             mutable_basis,
+                             immutable_basis){
+  m <- dim(sMat)[2]
+  n <- dim(sMat)[1]
+  k <- length(immutable_basis)
   new_basis <- c(sort(mutable_basis), immutable_basis)
   sMat <- transform.sMat(sMat, new_basis)
   S1 <- sMat[1:(n-k),,drop=FALSE][,1:(m-k),drop=FALSE]
   S2 <- sMat[1:(n-m),,drop=FALSE][,(m-k+1):m,drop=FALSE]
   determined <- setdiff(1:n, new_basis)
   mutable_series <- c(determined, mutable_basis)
+  
   mutable_weight <- solve(weighting_matrix[mutable_series,,drop=FALSE][,mutable_series,drop=FALSE])
   mutable_base <- cbind(base_forecasts[,determined,drop=FALSE] - t(S2 %*% t(base_forecasts[,immutable_basis,drop=FALSE])),
                         base_forecasts[,sort(mutable_basis),drop=FALSE])
   reconciled_mutable <- solve(t(S1) %*% mutable_weight %*% S1) %*% t(S1) %*% mutable_weight %*% t(mutable_base)
   reconciled_y <- t(sMat %*% rbind(reconciled_mutable, t(base_forecasts[,immutable_basis,drop=FALSE])))
-  mutable_weight <- mutable_weight / max(diag(mutable_weight))
-  if (nonnegative){
-    for (i in 1:dim(mutable_base)[1]){
-      Dmat <- t(S1) %*% mutable_weight %*% S1
-      dvec <- as.vector(t(mutable_base[i,]) %*% mutable_weight %*% S1)
-      Amat <- diag(rep(1, dim(S1)[2]))
-      bvec <- rep(0, dim(S1)[2])
-      sol <- try(quadprog::solve.QP(Dmat, dvec, Amat, bvec)$solution)
-      if (is(sol, "try-error")){
-        warning(paste0("unsolvable at row ", rownames(basef)[i], " use unconstrained solution!"))
-      }else{
-        reconciled_y[i,] <- as.vector(sMat %*% c(sol, base_forecasts[i,immutable_basis,drop=FALSE]))
-      }
-    }
-  }
   new_index <- c(determined, new_basis)
-  reconciled_y[,order(new_index)]
+  return(reconciled_y[,order(new_index)])
+}
+
+# Set constant
+evalN<-10 #Number of evaluation periods
+Q<-1000 #Number of draws to estimate energy score
+N<-500 #Training sample size
+M<-7 #Number of series
+
+#Set up S matrix
+S<-matrix(c(1,1,1,1,
+            1,1,0,0,
+            0,0,1,1,
+            1,0,0,0,
+            0,1,0,0,
+            0,0,1,0,
+            0,0,0,1),7,4,byrow = T)
+
+#Predefine some reconciliation matrices
+
+SG_bu<-S%*%cbind(matrix(0,4,3),diag(rep(1,4)))
+SG_ols<-S%*%solve(t(S)%*%S,t(S))
+
+# Set the type of the input dataset
+innovationsj<-'gaussian'
+depj<-'independent'
+
+#Read in data
+data<-read.csv('D:\\HierarchicalCode\\simulation\\Data\\Simulated_Data_With_Added_Noise.csv')
+
+# Read the Base forecast results
+fc<-fromJSON(file = "D:\\HierarchicalCode\\simulation\\Base_Forecasts\\base_forecasts_results.json")
+for (i in 1:10){
+  names(fc[[i]])<-c('fc_mean','fc_var','resid','fitted')
+}
+
+# Calculate sd
+for (i in 1:10){
+  sd_list<-NULL
+  for (j in 1:7){
+    sd_list<-c(sd_list,sqrt(fc[[i]]$fc_var[j]))
+  }
+  fc[[i]]$fc_sd<-sd_list
 }
 
 
+for (i in 1:10){
+  k<-NULL
+  for (j in 1:7){
+    m<-matrix(fc[[i]]['resid'][[1]][[j]],nrow=1,ncol=500,byrow=T)
+    k<-rbind(k,m)
+  }
+  fc[[i]]$resid_mat<-k
+  fc[[i]]$fc_Sigma_sam<-cov(t(k))
+  fc[[i]]$fc_Sigma_shr<-shrink.estim(t(k))
+}
+
+# Without immutable series
+Base<-rep(NA,evalN)
+BottomUp<-rep(NA,evalN)
+OLS<-rep(NA,evalN)
+WLS<-rep(NA,evalN)
+JPP<-rep(NA,evalN)
+MinTShr<-rep(NA,evalN)
+MinTSam<-rep(NA,evalN)
+BTTH<-rep(NA,evalN)
+Im<-rep(NA,evalN)
+
+# With immutable series
+OLSv<-rep(NA,evalN)
+WLSv<-rep(NA,evalN)
+MinTShrv<-rep(NA,evalN)
+MinTSamv<-rep(NA,evalN)
+
+
 # Start creating and evaluating
+# Set seed
+set.seed(12)
 
 for (i in 1:evalN){
   #Get realisation
@@ -221,6 +216,9 @@ for (i in 1:evalN){
     x<-t(rmvnorm(Q,fc_mean,fc_sigma))
     xs<-t(rmvnorm(Q,fc_mean,fc_sigma))
   }
+  print(x[1,1],xs[1,1])
+  # Set the immutable point and Get the basis series
+  basis_lis<-forecast.basis_series(S,immu_set=c(1))
   
   #Base forecast
   Base[i]<-energy_score(y,x,xs)
@@ -230,56 +228,71 @@ for (i in 1:evalN){
   
   #OLS
   OLS[i]<-energy_score(y,SG_ols%*%x,SG_ols%*%xs)
-  newx = t(forecast.reconcile(t(x), 
+  newx <- t(forecast.reconcile(t(x), 
                             S, 
                             diag(rep(1,M)),
-                            immu_set = c(1)))
-  newxs = t(forecast.reconcile(t(xs), 
+                            basis_lis$mutable_basis,
+                            basis_lis$immutable_basis))
+  newxs <- t(forecast.reconcile(t(xs), 
                             S, 
                             diag(rep(1,M)),
-                            immu_set = c(1)))
+                            basis_lis$mutable_basis,
+                            basis_lis$immutable_basis))
   OLSv[i]<-energy_score(y,newx,newxs)
-}
-  '''
-  
   
   #WLS (structural)
   SW_wls<-solve(diag(rowSums(S)),S)
   SG_wls<-S%*%solve(t(SW_wls)%*%S,t(SW_wls))
   WLS[i]<-energy_score(y,SG_wls%*%x,SG_wls%*%xs)
-  #WLSv[i]<-variogram_score(y,SG_wls%*%x,SG_wls%*%xs)
+  newx <- t(forecast.reconcile(t(x), 
+                              S, 
+                              diag(rowSums(S)),
+                              basis_lis$mutable_basis,
+                              basis_lis$immutable_basis))
+  newxs <- t(forecast.reconcile(t(xs), 
+                               S, 
+                               diag(rowSums(S)),
+                               basis_lis$mutable_basis,
+                               basis_lis$immutable_basis))
+  WLSv[i]<-energy_score(y,newx,newxs)
   
   #JPP
   JPP[i]<-energy_score(y,SG_wls%*%t(apply(x,1,sort)),SG_wls%*%t(apply(xs,1,sort)))
-  #JPPv[i]<-variogram_score(y,SG_wls%*%t(apply(x,1,sort)),SG_wls%*%t(apply(xs,1,sort)))
+  
+  #MinT (sam)
+  SW_MinTSam<-solve(fc_i$fc_Sigma_sam,S)
+  SG_MinTSam<-S%*%solve(t(SW_MinTSam)%*%S,t(SW_MinTSam))
+  MinTSam[i]<-energy_score(y,SG_MinTSam%*%x,SG_MinTSam%*%xs)
+  newx <- t(forecast.reconcile(t(x), 
+                               S, 
+                               fc_i$fc_Sigma_sam,
+                               basis_lis$mutable_basis,
+                               basis_lis$immutable_basis))
+  newxs <- t(forecast.reconcile(t(xs), 
+                                S, 
+                                fc_i$fc_Sigma_sam,
+                                basis_lis$mutable_basis,
+                                basis_lis$immutable_basis))
+  MinTSamv[i]<-energy_score(y,newx,newxs)
   
   #MinT (shr)
   SW_MinTShr<-solve(fc_i$fc_Sigma_shr,S)
   SG_MinTShr<-S%*%solve(t(SW_MinTShr)%*%S,t(SW_MinTShr))
   MinTShr[i]<-energy_score(y,SG_MinTShr%*%x,SG_MinTShr%*%xs)
-  #MinTShrv[i]<-variogram_score(y,SG_MinTShr%*%x,SG_MinTShr%*%xs)
+  newx <- t(forecast.reconcile(t(x), 
+                               S, 
+                               fc_i$fc_Sigma_shr,
+                               basis_lis$mutable_basis,
+                               basis_lis$immutable_basis))
+  newxs <- t(forecast.reconcile(t(xs), 
+                                S, 
+                                fc_i$fc_Sigma_shr,
+                                basis_lis$mutable_basis,
+                                basis_lis$immutable_basis))
+  MinTShrv[i]<-energy_score(y,newx,newxs)
+}
 
-  #MinT (sam)
-  SW_MinTSam<-solve(fc_i$fc_Sigma_sam,S)
-  SG_MinTSam<-S%*%solve(t(SW_MinTSam)%*%S,t(SW_MinTSam))
-  MinTSam[i]<-energy_score(y,SG_MinTSam%*%x,SG_MinTSam%*%xs)
-  #MinTSamv[i]<-variogram_score(y,SG_MinTSam%*%x,SG_MinTSam%*%xs)
-  
-  
-  # imut
-  shrinkagex = forecast.reconcile(x, 
-                                sMat, 
-                                fc_i$fc_Sigma_sam,
-                                immu_set = c(1))
-  shrinkagexs = forecast.reconcile(xs, 
-                                  sMat, 
-                                  fc_i$fc_Sigma_sam,
-                                  immu_set = c(1))
-  Imu[i]<-energy_score(y,shrinkagex,shrinkagexs)
-  '''
-
-res_independent<-data.frame(Base=Base,BottomUp=BottomUp,OLS=OLS,OLSv=OLSv)
-res_independent<-data.frame(Base=Base,BottomUp=BottomUp,JPP=JPP,OLS=OLS,WLS=WLS,MinTSam=MinTSam,MinTShr=MinTShr,Imu=Imu)
+res_independent<-data.frame(Base=Base,BottomUp=BottomUp,JPP=JPP,OLS=OLS,OLSv=OLSv,
+                            WLS=WLS,WLSv=WLSv,MinTSam=MinTSam,MinTSamv=MinTSamv,
+                            MinTShr=MinTShr,MinTShrv=MinTShrv)
 write.csv(res_independent,'D:\\HierarchicalCode\\simulation\\Reconcile_and_Evaluation_R\\independent_v2.csv')
-#res_joint<-data.frame(Base=Base,BottomUp=BottomUp,JPP=JPP,OLS=OLS,WLS=WLS,MinTSam=MinTSam,MinTShr=MinTShr,Imu=Imu)
-#write.csv(res_joint,'D:\\HierarchicalCode\\simulation\\Reconcile_and_Evaluation_R\\joint_v2.csv')
